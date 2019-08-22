7b008b97a21d3e8dc4f5c4ed86d635ae56a6bda5
koba-z33 <koba-z33@users.noreply.github.com>
2019-08-22 17:24:02 +0900

    Add CommandLine * line_no * raw_data * data * command_type

---
+++ projects/06/aasembler/python/n2tassembler/commandline.py
@@ -0,0 +1,81 @@
+from enum import Enum
+
+
+class CommandType(Enum):
+    A_COMMAND = 0
+    C_COMMAND = 1
+    L_COMMAND = 2
+    BLANK_LINE = -1
+
+
+class CommandLine():
+    """アセンブラコマンドラインオブジェクト
+
+    """
+
+    def __init__(self, line_no: int, raw_data: str):
+        """コンストラクタ
+
+        Parameters
+        ----------
+        line_no : int
+            行番号
+        raw_data : str
+            行生データ
+        """
+        self.__line_no = line_no
+        self.__raw_data = raw_data
+        self.__data = self.raw_data.split('//', 1)[0].strip()
+
+    @property
+    def line_no(self) -> int:
+        """行番号
+
+        Returns
+        -------
+        int
+            行番号
+        """
+        return self.__line_no
+
+    @property
+    def raw_data(self) -> str:
+        """行生データ
+
+        Returns
+        -------
+        str
+            行生データ
+        """
+        return self.__raw_data
+
+    @property
+    def data(self) -> str:
+        """コマンドデータ
+
+        Returns
+        -------
+        str
+            コマンドデータ
+        """
+
+        return self.raw_data.split('//', 1)[0].strip()
+
+    @property
+    def command_type(self) -> CommandType:
+        """コマンドタイプ
+
+        Returns
+        -------
+        CommandType
+            コマンドタイプ
+        """
+        if len(self.__data) == 0:
+            return CommandType.BLANK_LINE
+        if self.__data[0] == '@':
+            return CommandType.A_COMMAND
+        elif self.__data[0] == '(':
+            return CommandType.L_COMMAND
+        else:
+            return CommandType.C_COMMAND
+
---
+++ projects/06/aasembler/python/tests/test_commandline.py
@@ -0,0 +1,48 @@
+import pytest
+
+from n2tassembler.commandline import CommandLine, CommandType
+
+
+def test_init():
+    """初期化テスト
+    """
+    commandline = CommandLine(0, "command")
+    assert commandline.line_no == 0
+    assert commandline.raw_data == "command"
+    assert commandline.data == "command"
+
+
+def test_init_strip():
+    """前後空白除去
+    """
+    commandline = CommandLine(1, "  command  \n")
+    assert commandline.line_no == 1
+    assert commandline.raw_data == "  command  \n"
+    assert commandline.data == "command"
+
+
+def test_init_ignorecomment():
+    """コメント除去
+    """
+    commandline = CommandLine(123, "  command  // comment\n")
+    assert commandline.line_no == 123
+    assert commandline.raw_data == "  command  // comment\n"
+    assert commandline.data == "command"
+
+
+@pytest.mark.parametrize('raw_data, command_type',
+                         [
+                             ('@123', CommandType.A_COMMAND),
+                             ('          @123', CommandType.A_COMMAND),
+                             ('@LABEL', CommandType.A_COMMAND),
+                             ('      @LABEL', CommandType.A_COMMAND),
+                             ('(LABEL)', CommandType.L_COMMAND),
+                             ('     (LABEL)', CommandType.L_COMMAND),
+                             ('', CommandType.BLANK_LINE),
+                             ('         ', CommandType.BLANK_LINE),
+                             ('   // comment', CommandType.BLANK_LINE),
+                             ('M=M+D', CommandType.C_COMMAND),
+                         ])
+def test_commandtype(raw_data, command_type):
+    commandline = CommandLine(0, raw_data)
+    assert commandline.command_type == command_type
