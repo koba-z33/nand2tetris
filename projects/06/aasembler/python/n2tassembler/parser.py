14efa9e848a43eda9fd190c176fa3c00b6fc44b0
koba-z33 <koba-z33@users.noreply.github.com>
2019-08-27 09:22:36 +0900

    change interface Parser

--- projects/06/aasembler/python/n2tassembler/parser.py
+++ projects/06/aasembler/python/n2tassembler/parser.py
@@ -35,56 +35,12 @@ class Parser():
         self.__memory_pos = self.__memory_pos + 1
 
     @property
-    def command_type(self) -> CommandType:
-        """現コマンドのコマンドタイプ
+    def command(self) -> CommandLine:
+        """現コマンド
 
         Returns
         -------
         str
-            コマンドタイプ
+            コマンド
         """
-        return self.__lines[self.__memory_pos].command_type
-
-    @property
-    def symbol(self) -> str:
-        """現コマンドのシンボルを返す
-
-        Returns
-        -------
-        str
-            シンボル
-        """
-        return self.__lines[self.__memory_pos].symbol
-
-    @property
-    def dest(self) -> str:
-        """destニーモニック
-
-        Returns
-        -------
-        str
-            destニーモニック
-        """
-        return self.__lines[self.__memory_pos].dest
-
-    @property
-    def comp(self) -> str:
-        """compニーモニック
-
-        Returns
-        -------
-        str
-            compニーモニック
-        """
-        return self.__lines[self.__memory_pos].comp
-
-    @property
-    def jump(self) -> str:
-        """jumpニーモニック
-
-        Returns
-        -------
-        str
-            jumpニーモニック
-        """
-        return self.__lines[self.__memory_pos].jump
+        return self.__lines[self.__memory_pos]
--- projects/06/aasembler/python/tests/test_parser.py
+++ projects/06/aasembler/python/tests/test_parser.py
@@ -20,49 +20,49 @@ def test_parser():
 
     # @2
     parser.advance()
-    assert parser.command_type == CommandType.A_COMMAND
-    assert parser.symbol == '2'
+    assert parser.command.command_type == CommandType.A_COMMAND
+    assert parser.command.symbol == '2'
 
     # D=A
     assert parser.has_more_commands is True
     parser.advance()
-    assert parser.command_type == CommandType.C_COMMAND
-    assert parser.dest == 'D'
-    assert parser.comp == 'A'
-    assert parser.jump == 'null'
+    assert parser.command.command_type == CommandType.C_COMMAND
+    assert parser.command.dest == 'D'
+    assert parser.command.comp == 'A'
+    assert parser.command.jump == 'null'
 
     # (LOOP)
     assert parser.has_more_commands is True
     parser.advance()
-    assert parser.command_type == CommandType.L_COMMAND
-    assert parser.symbol == "LOOP"
+    assert parser.command.command_type == CommandType.L_COMMAND
+    assert parser.command.symbol == "LOOP"
 
     # @3
     assert parser.has_more_commands is True
     parser.advance()
-    assert parser.command_type == CommandType.A_COMMAND
-    assert parser.symbol == '3'
+    assert parser.command.command_type == CommandType.A_COMMAND
+    assert parser.command.symbol == '3'
 
     # D=D+A
     assert parser.has_more_commands is True
     parser.advance()
-    assert parser.command_type == CommandType.C_COMMAND
-    assert parser.dest == 'D'
-    assert parser.comp == 'D+A'
-    assert parser.jump == 'null'
+    assert parser.command.command_type == CommandType.C_COMMAND
+    assert parser.command.dest == 'D'
+    assert parser.command.comp == 'D+A'
+    assert parser.command.jump == 'null'
 
     # @LOOP
     assert parser.has_more_commands is True
     parser.advance()
-    assert parser.command_type == CommandType.A_COMMAND
-    assert parser.symbol == 'LOOP'
+    assert parser.command.command_type == CommandType.A_COMMAND
+    assert parser.command.symbol == 'LOOP'
 
     # 0;JMP
     assert parser.has_more_commands is True
     parser.advance()
-    assert parser.command_type == CommandType.C_COMMAND
-    assert parser.dest == 'null'
-    assert parser.comp == '0'
-    assert parser.jump == 'JMP'
+    assert parser.command.command_type == CommandType.C_COMMAND
+    assert parser.command.dest == 'null'
+    assert parser.command.comp == '0'
+    assert parser.command.jump == 'JMP'
 
     assert parser.has_more_commands is False
