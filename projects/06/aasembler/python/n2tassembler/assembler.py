3bad02d42d50ba34ab6fbeda9afc0e90df264494
koba-z33 <koba-z33@users.noreply.github.com>
2019-08-27 09:43:07 +0900

    Add Assembler make hack file name

--- projects/06/aasembler/python/n2tassembler/assembler.py
+++ projects/06/aasembler/python/n2tassembler/assembler.py
@@ -1,3 +1,5 @@
+import os
+
 from . import CommandLine, CommandType, CommandLineError, Code
 
 
@@ -9,17 +11,17 @@ class Assembler():
 
     def make_binary(self, commandline: CommandLine) -> str:
         """コマンドから売那智コード変換
-        
+
         Parameters
         ----------
         commandline : CommandLine
             コマンドライン
-        
+
         Returns
         -------
         str
             バイナリコード
-        
+
         Raises
         ------
         CommandLineError
@@ -35,3 +37,18 @@ class Assembler():
             return self.__code.num2bin(int(commandline.symbol))
         else:
             raise CommandLineError(str(commandline))
+
+    def hack_filename(self, asm_filename: str) -> str:
+        """hackファイル名作成
+        
+        Parameters
+        ----------
+        asm_filename : str
+            asmファイル名
+        
+        Returns
+        -------
+        str
+            hackファイル名
+        """
+        return os.path.splitext(asm_filename)[0] + '.hack'
--- projects/06/aasembler/python/tests/test_assembler.py
+++ projects/06/aasembler/python/tests/test_assembler.py
@@ -3,19 +3,30 @@ import pytest
 from n2tassembler import CommandLine, Assembler
 
 
-@pytest.mark.parametrize('command, machine',
-                         [
-                             ('@0', '0000000000000000'),
-                             ('@1', '0000000000000001'),
-                             ('D=A', '1110110000010000'),
-                             ('M=A', '1110110000001000'),
-                             ('D=D+A', '1110000010010000'),
-                             ('D=D+M', '1111000010010000'),
-                             ('0;JMP', '1110101010000111'),
-                             ('0;JNE', '1110101010000101'),
-                         ])
+@pytest.mark.parametrize('command, machine', [
+    ('@0', '0000000000000000'),
+    ('@1', '0000000000000001'),
+    ('D=A', '1110110000010000'),
+    ('M=A', '1110110000001000'),
+    ('D=D+A', '1110000010010000'),
+    ('D=D+M', '1111000010010000'),
+    ('0;JMP', '1110101010000111'),
+    ('0;JNE', '1110101010000101'), ])
 def test_assmble(command, machine):
     assembler = Assembler()
 
     commandline = CommandLine(0, command)
     assert assembler.make_binary(commandline) == machine
+
+
+@pytest.mark.parametrize('asm_filename, hack_filename', [
+    ('/home/hogege/plus.asm', '/home/hogege/plus.hack'),
+    ('hogege/plus.asm', 'hogege/plus.hack'),
+    ('./hogege/plus.asm', './hogege/plus.hack'),
+    ('plus.asm', 'plus.hack'),
+    ('C:/home/hogege/plus.asm', 'C:/home/hogege/plus.hack'),
+    ('C:\\home\\hogege\\plus.asm', 'C:\\home\\hogege\\plus.hack'), ])
+def test_hack_filename(asm_filename, hack_filename):
+    assembler = Assembler()
+
+    assert assembler.hack_filename(asm_filename) == hack_filename
