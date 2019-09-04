b2cb884796b864bd4624ceb62de3be0d87f6b063
koba-z33 <koba-z33@users.noreply.github.com>
2019-09-04 17:25:51 +0900

    make ch7 translate bat

---
+++ projects/07/translate.bat
@@ -0,0 +1,5 @@
+VMtranslator.exe .\StackArithmetic\SimpleAdd\SimpleAdd.vm
+VMtranslator.exe .\StackArithmetic\StackTest\StackTest.vm
+VMtranslator.exe .\MemoryAccess\BasicTest\BasicTest.vm
+VMtranslator.exe .\MemoryAccess\PointerTest\PointerTest.vm
+VMtranslator.exe .\MemoryAccess\StaticTest\StaticTest.vm
