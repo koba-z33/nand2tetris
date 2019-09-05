ecf7ff7edbce339d896edd6efaca4467f7899ca9
koba-z33 <koba-z33@users.noreply.github.com>
2019-09-05 09:48:12 +0900

    Add Vmtranslator for Directory

--- projects/python/vmtranslator/n2tvmtranslator/vmtranslator.py
+++ projects/python/vmtranslator/n2tvmtranslator/vmtranslator.py
@@ -8,8 +8,11 @@ class VMtranslator():
     def __init__(self):
         self.__codewriter = CodeWriter()
 
-    def translate(self, lines: list) -> str:
-        parser: Parser = Parser(lines)
+    def translate(self, filename: str) -> str:
+        with open(filename, 'r') as f:
+            vm_lines = f.readlines()
+
+        parser: Parser = Parser(vm_lines)
 
         asm = ''
         while parser.has_more_commands:
@@ -19,33 +22,47 @@ class VMtranslator():
 
         return asm
 
-    def asm_filename(self, vm_filename: str) -> str:
-        """asmファイル名作成
+    def make_file_info(self, argv: str) -> dict:
+        """VMtranslatorの入出力ファイル情報の作成
 
         Parameters
         ----------
-        vm_filename : str
-            vmファイル名
+        argv : str
+            コマンドライン引数
 
         Returns
         -------
-        str
-            asmファイル名
+        dict
+            入出力情報dict
         """
-        return os.path.splitext(vm_filename)[0] + '.asm'
+        import glob
+
+        abs_path = os.path.abspath(argv)
+        asm_file = os.path.splitext(abs_path)[0] + '.asm'
+        if os.path.isfile(abs_path):
+            vm_files = [abs_path]
+        else:
+            vm_files = glob.glob(os.path.join(abs_path, '*.vm'))
+
+        return {
+            'asm_file': asm_file,
+            'vm_files': vm_files,
+        }
 
 
 def main():
+    argv: str = sys.argv[1]
+
     vmtranslator = VMtranslator()
 
-    vm_filename: str = sys.argv[1]
-    asm_filename: str = vmtranslator.asm_filename(vm_filename)
-    print(f'VMtranslate {vm_filename} -> {asm_filename}')
+    file_info = vmtranslator.make_file_info(argv)
 
-    with open(vm_filename, 'r') as f:
-        vm_lines = f.readlines()
+    print(f'VMtranslate {argv} -> {file_info["asm_file"]}')
 
-    asm = vmtranslator.translate(vm_lines)
+    asm = ''
+    for vm_file in file_info['vm_files']:
+        print(f'translate {vm_file}')
+        asm = asm + vmtranslator.translate(vm_file)
 
-    with open(asm_filename, 'w') as f:
+    with open(file_info['asm_file'], 'w') as f:
         f.write(asm)
